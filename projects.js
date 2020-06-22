2d5bf78454ed7b2732244c3483dadda7ffd08869
Camille Kulanda <camille.kulanda@siu.edu>
2020-06-21 20:27:13 -0500

    Add files via upload

--- projects.js
+++ projects.js
@@ -1,35 +1,4 @@
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
 var modal = document.getElementById("myModal");
-var modal = document.getElementById("myModal2");
-var modal = document.getElementById("myModal3");
-var modal = document.getElementById("myModal4");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
@@ -41,49 +10,10 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
-var img = document.getElementById("myImg2");
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-var img = document.getElementById("myImg3");
-var modalImg = document.getElementById("img03");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-var img = document.getElementById("myImg4");
-var modalImg = document.getElementById("img04");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-var span = document.getElementsByClassName("close")[1];
-var span = document.getElementsByClassName("close")[2];
-var span = document.getElementsByClassName("close")[3];
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 span.onclick = function() {
   modal.style.display = "none";
 }
